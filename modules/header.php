b04626fb1147db8a399fa85e9a43898f919788c5
Dzonint <dzonint@yahoo.com>
2017-09-05 17:49:19 +0200

    Update header.php

--- modules/header.php
+++ modules/header.php
@@ -4,6 +4,7 @@
   <title>eveNT Belgrade</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="img/icon.png">    
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">        
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/navbar.css">  
